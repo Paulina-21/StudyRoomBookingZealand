8ac9eada998265b9db5189136eb73911c7e38e80
Paula Catalina Curca <paul6465@edu.easj.dk>
2021-04-22 09:11:08 +0200

    Created the Models based on the requirments we started writing yesterday. I think we should do code first, not DB.

---
+++ Models/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyroomBookingZealand.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { set; get; }
+        public string Name { set; get; }
+        public string Address { set; get; }
+        public string PhoneNr { set; get; }
+        public string Email { set; get; }
+        public string Username { set; get; }
+        public string Password { set; get; }
+        public object Role { set; get; }  
+    }
+}
