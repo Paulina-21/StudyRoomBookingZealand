4950a556a187beff7bebdcd609ab63e0c227c1be
Paula Catalina Curca <paul6465@edu.easj.dk>
2021-05-05 15:38:47 +0200

    Smth wrong

--- Models/BookingDbContext.cs
+++ Models/BookingDbContext.cs
@@ -8,11 +8,32 @@ using Microsoft.EntityFrameworkCore;
 
 namespace StudyroomBookingZealand.Models
 {
-    public class BookingDbContext: DbContext
+    public class BookingDbContext : DbContext
     {
-        public BookingDbContext() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https: //go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer(
+                    "Data Source=(localdb)\\MSSQLLocaldb;Initial Catalog=BookingDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //{
+        //    options.UseSqlServer(
+        //        @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BookingDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        //}
+        public BookingDbContext()
+        {
+        }
+
         public BookingDbContext(DbContextOptions options) : base(options)
-        { }
+        {
+        }
+
         public virtual DbSet<User> Users { set; get; }
         public virtual DbSet<Booking> Bookings { get; set; }
         public virtual DbSet<Group> Groups { get; set; }
@@ -20,13 +41,5 @@ namespace StudyroomBookingZealand.Models
         public virtual DbSet<Location> Locations { set; get; }
         public virtual DbSet<Admin> Teachers { set; get; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocaldb;Initial Catalog=BookingDB;Integrated Security=True");
-            }
-        }
     }
 }
