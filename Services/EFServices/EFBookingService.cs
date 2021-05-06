53286dc8f88dab3f61460ce2d2d058a78521e060
pc <pc@DESKTOP-BI5FP4B>
2021-05-06 19:19:30 +0200

    Added BookingService and changed 1 word

---
+++ Services/EFServices/EFBookingService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StudyroomBookingZealand.Models;
+using StudyroomBookingZealand.Services.Interfaces;
+
+namespace StudyroomBookingZealand.Services.EFServices
+{
+    public class EFBookingService : IBookings
+    {
+        private BookingDbContext _service;
+        public EFBookingService(BookingDbContext db)
+        {
+            _service = db;
+        }
+
+        public void AddBooking(Booking b)
+        {
+            _service.Bookings.Add(b);
+            _service.SaveChanges();
+        }
+
+        public void DeleteBooking(int id)
+        {
+            _service.Bookings.Remove(GetBookingById(id));
+            _service.SaveChanges();
+        }
+
+        public List<Booking> GetAllBookings()
+        {
+            return _service.Bookings.ToList();
+        }
+
+        public Booking GetBookingById(int id)
+        {
+            return _service.Bookings.Find(id);
+        }
+
+        public void UpdateGroup(int id)
+        {
+            _service.Bookings.Update(GetBookingById(id));
+            _service.SaveChanges();
+        }
+    }
+}
