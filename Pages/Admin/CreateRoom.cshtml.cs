164262fb65a95a71f912de5918108f14436f88c0
pc <pc@DESKTOP-BI5FP4B>
2021-05-16 00:19:05 +0200

    Added CreateRoom with Location Selector, Added Room Counter and Booking Counter and on the RoomList i replaced the location with the Location Name

--- Pages/Admin/CreateRoom.cshtml.cs
+++ Pages/Admin/CreateRoom.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using StudyroomBookingZealand.Models;
 using StudyroomBookingZealand.Services.Interfaces;
 
@@ -13,21 +14,29 @@ namespace StudyroomBookingZealand.Pages.Admin
     {
         [BindProperty]
         public Room Room { set; get; }
+        [BindProperty]
+        public int LocationId { get; set; }
+        public SelectList Options { get; set; }
+       
         private IRoom _roomService;
+        private ILocations _locationsService;
 
-        public CreateRoomModel(IRoom room)
+        public CreateRoomModel(IRoom room, ILocations locations)
         {
             _roomService = room;
+            _locationsService = locations;
         }
 
         public void OnGet()
         {
+            Options = new SelectList(_locationsService.GetAllLocations(), nameof(Location.LocationId), nameof(Location.Name));
         }
 
         public IActionResult OnPost()
         {
+            Room.LocationId = LocationId;
             _roomService.AddRoom(Room);
-            return Page();
+            return Redirect("/Locations/Rooms/ListRooms");
         }
     }
 }
