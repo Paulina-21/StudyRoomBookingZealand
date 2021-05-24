7b37a4cf5485002aa16f94dc665d64b590888417
pc <pc@DESKTOP-BI5FP4B>
2021-05-24 14:03:08 +0200

    Added email message, added property to user to store warnings

--- Pages/Bookings/DeleteBooking.cshtml.cs
+++ Pages/Bookings/DeleteBooking.cshtml.cs
@@ -39,15 +39,28 @@ namespace StudyroomBookingZealand.Pages.Bookings
 
         public async Task<IActionResult> OnPost(int id)
         {
+            
             List<string> receivers = Data.Helpers.EmailHelper.GatherEmails(_bookingService.BookingOwners(id));
-            string subject = "My back aches";
-            string content = "<h1>Free robux down below!!<h1>";
+            string subject = "Warning - Your booked room will be deleted in 3 days";
+            string content = $"<h1>Your booking on the {_bookingService.GetBookingById(id).FromDateTime} will be deleted in 3 days </h1>" +
+                             $"<a> Due to an unforeseen circumstance, your booking will be removed in 3 days by an Administrator, we are sorry for the inconvenience </a>";
+
+            List<Models.User> bookingMembers = _bookingService.BookingOwners(id);
+
+            foreach (var user in bookingMembers)
+            {
+                //booking warning, maybe set property
+            }
+
             Data.Helpers.EmailHelper.SendEmail(receivers, subject, content);
 
             //Async programming piece of code. It creates a new task that will be executed in 72 hours. In this case it will delete a booking in 3 days.
             // Fixed by changing method to async
-            await Task.Delay(new TimeSpan(72, 0, 0)).ContinueWith(o => { _bookingService.DeleteBooking(id); });
-            return Redirect("/Bookings/ListBookings");
+            await Task.Delay(new TimeSpan(72, 0, 0)).ContinueWith(o =>
+            {
+                _bookingService.DeleteBooking(id);
+            });
+            return Redirect("/Admin/ListBookings");
         }
     }
 }
